03b88d37def1133cfb35bf261c37cd059ab44423
Vincent Léon <vincent.leon@icloud.com>
2020-07-06 21:10:48 +0200

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Vincent Léon on 06/07/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/goshinAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  goshinAppApp.swift
+//  Shared
+//
+//  Created by Vincent Léon on 06/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct goshinAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
